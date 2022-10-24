6e06e395e8cd3c6a0b42808b6cf2f0175b834369
HPavlova <77845327+HPavlova@users.noreply.github.com>
2022-10-24 20:07:28 +0300

    Update admin.py

--- my_finance/transactions/admin.py
+++ my_finance/transactions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from models import CategoryIncome, CategoryExpense, Income, Expense
 
 # Register your models here.
+admin.site.register(CategoryIncome)
+admin.site.register(CategoryExpense)
+admin.site.register(Income)
+admin.site.register(Expense)
