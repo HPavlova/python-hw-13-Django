28aac2512cc315e947b0cb9319fc5f83813304d2
HPavlova <77845327+HPavlova@users.noreply.github.com>
2022-10-25 21:39:47 +0300

    Update views.py

--- my_finance/user/views.py
+++ my_finance/user/views.py
@@ -1,9 +1,83 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django import forms
+from django.contrib import messages
+from django.urls import reverse
+from faker import Faker
+fake = Faker()
+
 
 # Create your views here.
+class RegistrationForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Name', 'style': 'width: 300px;', 'class': 'form-control'}))
+    password = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Password', 'style': 'width: 300px;', 'class': 'form-control'}))
+    email = forms.EmailField()
 
-from django.http import HttpResponse
+
+class BadRequestException(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+
+def _validate_data(username, password):
+    error = None
+    if not username:
+        raise BadRequestException('Username is required!')
+    if not password:
+        raise BadRequestException('Password is required!')
+    return error
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the user index.")
+    return redirect('auth:login')
+
+
+def register_view(request):
+    if request.method == 'GET':
+        return render(request, 'auth/register.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            _validate_data(username, password)
+        except BadRequestException as e:
+            messages.add_message(request, messages.ERROR, e.msg)
+            return render(request, 'auth/register.html')
+        user_exists = User.objects.filter(username=username).exists()
+        if user_exists:
+            messages.add_message(request, messages.ERROR, 'User already exists')
+            return render(request, 'auth/register.html')
+        else:
+            user = User.objects.create_user(username=username, email=fake.email(), password=password)
+            user.save()
+            return redirect('auth:login')
+
+
+def login_view(request):
+    if request.method == 'GET':
+        return render(request, 'auth/login.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            _validate_data(username, password)
+        except BadRequestException as e:
+            messages.add_message(request, messages.ERROR, e.msg)
+            return render(request, 'auth/login.html')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect(reverse(request, 'transactions:index'))
+        else:
+            messages.add_message(request, messages.ERROR, 'Invalid credentials')
+            return render(request, 'auth/login.html')
+
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('auth:login')
