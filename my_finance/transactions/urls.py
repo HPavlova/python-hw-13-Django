f9f5a6a27ed4f404a0fdd953c7dc8c52e9261fa5
HPavlova <77845327+HPavlova@users.noreply.github.com>
2022-10-26 22:29:00 +0300

    Update urls.py

--- my_finance/transactions/urls.py
+++ my_finance/transactions/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.transactions, name='index'),
+    path('report/', views.report, name='report'),
+    path('income/', views.create_income_transaction, name='income'),
+    path('expense/', views.create_expense_transaction, name='expense'),
+    path('delete_income/<int:income_id>/', views.delete_income_transaction, name='delete_income'),
+    path('delete_expense/<int:expense_id>/', views.delete_expense_transaction, name='delete_expense'),
+    path('filter/', views.filter_transaction, name='filter'),
 ]
