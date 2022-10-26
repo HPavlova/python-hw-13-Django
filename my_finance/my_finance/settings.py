0a565e24cf678cbd7d05d6e74b69621a5b93d632
HPavlova <77845327+HPavlova@users.noreply.github.com>
2022-10-26 22:25:52 +0300

    add static

--- my_finance/my_finance/settings.py
+++ my_finance/my_finance/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,13 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s+w0*gw9gsq-7b9j)1a*_co1bvbzi4h#wx8@i%r=65%bix!sz='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'white-wildflower-2076.fly.dev']
-
-CSRF_TRUSTED_ORIGINS = [
-    'https://white-wildflower-2076.fly.dev'
-]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Application definition
 
@@ -120,7 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
